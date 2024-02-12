ebb067763440f979e923cd8a58ed5d808401cef5
Yang, Yuchen <yangyuc@iu.edu>
2024-02-12 09:08:44 -0500

    coffee_order_LEC

--- src/main/java/edu/iu/habahram/coffeeorder/model/OrderData.java
+++ src/main/java/edu/iu/habahram/coffeeorder/model/OrderData.java
@@ -3,4 +3,12 @@ package edu.iu.habahram.coffeeorder.model;
 import java.util.List;
 
 public record OrderData(String beverage, List<String> condiments) {
+    public String toString() {
+        return "beverage=" + this.beverage + ", condiments=" + this.condiments;
+    }
+
+    public String fromLine(String line) {
+        String[] parts = line.split(", ");
+        return parts[0];
+    }
 }
