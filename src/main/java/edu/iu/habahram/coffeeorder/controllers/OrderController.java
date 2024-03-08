b319fc2621b4e16e707bfb54a132bb784f90da89
Yang, Yuchen <yangyuc@iu.edu>
2024-03-07 22:26:49 -0500

    A6

--- src/main/java/edu/iu/habahram/coffeeorder/controllers/OrderController.java
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/OrderController.java
@@ -5,13 +5,11 @@ import edu.iu.habahram.coffeeorder.model.Receipt;
 import edu.iu.habahram.coffeeorder.repository.OrderRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin
 public class OrderController {
     private OrderRepository orderRepository;
 
@@ -20,16 +18,8 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody OrderData order) {
-        try {
-            Receipt receipt = orderRepository.add(order);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(receipt);
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(e.getMessage());
-        }
+    public ResponseEntity<?> add(@RequestBody OrderData order) throws Exception {
+        Receipt receipt = orderRepository.add(order);
+        return new ResponseEntity<>(receipt, HttpStatus.CREATED);
     }
 }
